dbc7c59c2c5fa88c8c66323fcb03d972f8ad3e33
fjrjdev <thedromemafia@gmail.com>
2023-04-15 18:09:52 -0300

    feat: login service and refresh

--- src/routers/auth.router.ts
+++ src/routers/auth.router.ts
@@ -2,18 +2,20 @@ import { Router } from "express";
 import AuthService from "../services/auth.service";
 import { IUser } from "../models/user.model";
 
+const router = Router();
 
-const router = Router()
+router.post("/signup", async (req, res) => {
+  const data = await AuthService.create(req.body);
+  return res.status(201).send({ results: data });
+});
 
-router.post('/signup', async(req, res) => {
-    const data = await AuthService.create(req.body)
-    return res.status(201).send({results: data})
-} 
-)
+router.post("/signin", async (req, res) => {
+  const tokens = await AuthService.login(req.body);
+  return res.status(200).send({ ...tokens });
+});
+router.post("/refresh", async (req, res) => {
+    const accessToken = await AuthService.refresh(req.body);
+    return res.status(200).send({ ...accessToken });
+  });
 
-router.post('/signin', (req, res) => {
-    return res.status(200).send({message:'login'})
-} 
-)
-
-export default router
+export default router;
