cf5b73c3e12f5e1e5b4533cd53ec6991feb72a0d
fjrjdev <thedromemafia@gmail.com>
2023-04-15 13:27:57 -0300

    feat: tasklist schema model

---
+++ src/models/taskList.model.ts
@@ -0,0 +1,48 @@
+import mongoose from "mongoose";
+import { ITask } from "./task.model";
+import { IUser } from "./user.model";
+
+export interface ITaskList{
+  name: string
+  tasks: ITask[] 
+  sharedWidth: IUser[] 
+  createdBy: IUser
+  updatedBy: IUser
+  createdAt: string | Date;
+  updatedAt: string | Date;
+}
+const taskListSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  tasks: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Task'
+  }],
+  createdBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  updatedBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  sharedWith: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  }],
+  createdAt: {
+    type: Date,
+    default: new Date(),
+  },
+  updatedAt: {
+    type: Date,
+    default:new Date(),
+  },
+});
+
+
+export const TaskList = mongoose.model<ITaskList>("TaskList", taskListSchema)
+
