626e8ed57d053721f5658158bf34d471da723539
fjrjdev <thedromemafia@gmail.com>
2023-04-18 19:04:15 -0300

    docs: schemas resume

--- src/middlewares/error.middleware.ts
+++ src/middlewares/error.middleware.ts
@@ -1,24 +1,25 @@
-import { Request, Response, NextFunction } from 'express';
-import AppError from '../errors/appError';
+import { Request, Response, NextFunction } from "express";
+import AppError from "../errors/appError";
 
 export const errorMiddleware = (
+  /*Executado quando ocorre um erro*/
   err: any,
   request: Request,
   response: Response,
   _: NextFunction
 ) => {
-  console.log(err)
+  console.log(err);
   if (err instanceof AppError) {
     return response.status(err.statusCode).json({
-      status: 'error',
+      status: "error",
       code: err.statusCode,
       message: err.message,
     });
   }
 
   return response.status(500).json({
-    status: 'error',
+    status: "error",
     code: 500,
-    message: 'Internal server error',
+    message: "Internal server error",
   });
 };
--- src/middlewares/schemas.middleware.ts
+++ src/middlewares/schemas.middleware.ts
@@ -3,19 +3,21 @@ import AppError from "../errors/appError";
 import { AnySchema } from "yup";
 
 export const schemasMiddleware =
-  (schema: AnySchema) =>
-  async (req: Request, res: Response, next: NextFunction) => {
-    const data = req.body;
-    try {
-      await schema.validate(data, { abortEarly: false });
-      next();
-    } catch (err: any) {
-      const invalidFields = err.inner.map((innerErr:any) => {
-        const field = innerErr.path;
-        const message = innerErr.message;
-        return `${field}: ${message}`;
-      });
-      const message = `Invalid fields: ${invalidFields.join(", ")}`;
-      throw new AppError(400, message);
-    }
-  };
+  /* Valida o corpo da solicitação HTTP*/
+
+    (schema: AnySchema) =>
+    async (req: Request, res: Response, next: NextFunction) => {
+      const data = req.body;
+      try {
+        await schema.validate(data, { abortEarly: false });
+        next();
+      } catch (err: any) {
+        const invalidFields = err.inner.map((innerErr: any) => {
+          const field = innerErr.path;
+          const message = innerErr.message;
+          return `${field}: ${message}`;
+        });
+        const message = `Invalid fields: ${invalidFields.join(", ")}`;
+        throw new AppError(400, message);
+      }
+    };
